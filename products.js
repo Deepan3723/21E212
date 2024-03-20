3ed9fe4becdbde6a93fc9eaec002dcd62208a0af
Deepan S <131612843+Deepan3723@users.noreply.github.com>
2024-03-20 12:17:58 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import React, { useState } from 'react';
+function Product(){
+    const url="https://20.244.56.144/products/register";
+    const [product,setProduct]= useState([
+        {
+            "companyName": "AFFORDMED",
+            "ownerName": "Deepan",
+            "rollNo": "717821E212",
+            "ownerEmail": "deepan3723@gmail.com",
+            "accssCode": "qaNXGH"
+        }
+    ])
+}
